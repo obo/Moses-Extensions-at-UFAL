d420058077dab0a30863ba00679aaaed42725855
Ondrej Bojar <bojar@tauri4.ufal.hide.ms.mff.cuni.cz.ufal.hide.ms.mff.cuni.cz>
2010-01-26 23:16:12 +0100

    fixed a minor bug introduced by previous commit

--- moses/src/SearchNormal.cpp
+++ moses/src/SearchNormal.cpp
@@ -23,7 +23,9 @@ SearchNormal::SearchNormal(const InputType &source, const TranslationOptionColle
 	// only if constraint decoding (having to match a specified output)
 	long sentenceID = source.GetTranslationId();
 	m_constraint = staticData.GetConstrainingPhrase(sentenceID);
-        VERBOSE(1, "Search constraint to output: " << *m_constraint<<endl);
+        if (m_constraint) {
+          VERBOSE(1, "Search constraint to output: " << *m_constraint<<endl);
+        }
 
 	// initialize the stacks: create data structure and set limits
 	std::vector < HypothesisStackNormal >::iterator iterStack;
