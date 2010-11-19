b68c82588e507fff6ffd7c7a70eb4b5ae8bc89cc
Ondrej Bojar <bojar@ufal.mff.cuni.cz>
2010-11-19 01:31:51 +0100

    re-adding files that were introduced in moses-svn development

---
+++ kenlm/util/scoped.cc
@@ -0,0 +1,16 @@
+#include "util/scoped.hh"
+
+#include <err.h>
+#include <unistd.h>
+
+namespace util {
+
+scoped_fd::~scoped_fd() {
+  if (fd_ != -1 && close(fd_)) err(1, "Could not close file %i", fd_);
+}
+
+scoped_FILE::~scoped_FILE() {
+  if (file_ && fclose(file_)) err(1, "Could not close file");
+}
+
+} // namespace util
